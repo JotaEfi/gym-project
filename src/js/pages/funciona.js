101b84b6bd5f83e6aa9bdbcb02e6226dd830f239
João Filho <joaodaviabc@gmail.com>
2023-12-08 12:19:24 -0300

    Pages organization

---
+++ src/js/pages/funciona.js
@@ -0,0 +1,8 @@
+import '../../css/style.css';
+import '../../css/queries.css';
+
+function Funciona() {
+  return <div className="container"></div>;
+}
+
+export default Funciona;
---
+++ src/js/pages/resultados.js
@@ -0,0 +1,8 @@
+import '../../css/style.css';
+import '../../css/queries.css';
+
+function Resultados() {
+  return <div className="container"></div>;
+}
+
+export default Resultados;
